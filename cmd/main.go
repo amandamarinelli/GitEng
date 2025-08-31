68cb3b1051cadf729282f2a7d868d8b71423871d
Amanda Marinelli <amarinellif@192.168.0.8>
2025-08-31 15:17:23 -0300

    chore: add basic folders

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	router := gin.Default()
+const gitClientIdName = "GITHUB_CLIENT_ID"
+const gitSecretName = "GITHUB_CLIENT_ID"
 
+func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	gitClientId := os.Getenv(gitClientIdName)
+	gitSecret := os.Getenv(gitSecretName)
+
+	if gitClientId == "" || gitSecret == "" {
+		panic("GitHub client ID or secret not set in environment variables")
+	}
+
+	// Initialize Router
+	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	err := router.Run()
+	// Start server
+	err = router.Run()
 	if err != nil {
 		panic(err)
 	}
+
 }
